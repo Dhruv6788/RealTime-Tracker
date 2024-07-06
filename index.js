e9620ab6c06d4b4749a2e1dd2cd5696128baddd7
Dhruv Joshi <jdhruv036@gmail.com>
2024-07-06 14:18:57 +0530

    First Commit!!

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("send-location", (data) => {
+    console.log(data);
+    io.emit('receive-location', {id : socket.id, ...data})
+  });
+
+  socket.on("disconnect", () => {
+    io.emit('user-disconnected', socket.id)
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(process.env.PORT, ()=> {
+  console.log("Running")
+});
